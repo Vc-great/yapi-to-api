db563e8d0f45b0bad39ea11b6302bfcfe9e648fd
vc <znro0001@126.com>
2023-05-20 20:09:08 +0800

    feat:  json schema

--- template/config.js
+++ template/config.js
@@ -1,6 +1,8 @@
 const temp = `
 module.exports = {
     yapiUrl:'',  // example: www.baidu.com
+    typeResponseArray:false,  //返回值ts类型是否为数组 false: Promise<response> true: [object,Promise<response>]
+    jsonSchema:true,  //是否生成jsonSchema
     projects:[
         {
             title:'',  //项目名称 
